dd0ea7382402f57604c440afd56d505c8cac46f2
Chris Cowperthwait <ccowperthwait@transperfect.com>
2018-06-22 15:13:07 -0600

    Checking in some initial changes.

---
+++ assignment/src/OutputTextArea/OutputTextArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputTextArea = () => {
+    return (
+        <div>
+            <p>Information should appear here.</p>
+        </div>
+    )
+}
+
+export default OutputTextArea;
